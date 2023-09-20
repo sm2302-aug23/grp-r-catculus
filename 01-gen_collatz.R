b8e26ddd1d3a083555bcbf3432a0856f55d9df83
suriyna <20b2067@ubd.edu.bn>
2023-09-20 17:11:41 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -1,1 +1,35 @@
 # Task 1 
+
+# Load the necessary libraries
+library(tidyverse)
+library(tibble)
+
+# Set the functions
+is.even <- function(x) {
+  if (x %% 2 == 0) TRUE
+  else FALSE
+}
+
+# is.even(2)
+# is.even(5)
+
+gen_collatz <- function(n) {
+  if (is.even(n)) n/2
+  else 3 * n + 1
+}
+
+# gen_collatz(5)
+# gen_collatz(27)
+
+# Apply function to all integers from 1-10,000 and store in a tibble
+start <- 1:10000
+
+n_total <- n <- 36 #any random number
+while (n !=1) {
+  n <- gen_collatz(n)
+  n_total <- c(n_total, n)
+}
+
+n_total
+
+
