c180029b420797ea62702c34977fc112eafa5c3f
syirahomar <20b2071@ubd.edu.bn>
2023-09-20 17:52:48 +0800

    added comments on task 1

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -16,15 +16,18 @@ is.even <- function(n) {
 
 gen_collatz <- function(n) {
   
+  # check if n is a positive integer
   if (n <= 0) {
     stop("Number should be a positive integer > 0")
   }
   
   while (n>1) {
-    
+  
+  # if n is even, the next term is one half of the previous term.  
   if (n %% 2 == 0) {
     n <- n/2 }
   
+  # if n is odd, the next term is 3 times the previous term plus 1.  
   else {
     n <- 3 * n + 1 }
   }
