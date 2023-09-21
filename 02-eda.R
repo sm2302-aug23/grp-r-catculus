c9db2a4c1be239537537ac77f653c5c83a5b9638
syirahomar <20b2071@ubd.edu.bn>
2023-09-21 16:38:25 +0800

    update task 2, no.1

--- 02-eda.R
+++ 02-eda.R
@@ -1,1 +1,12 @@
-# Task 2 
+# Task 2
+
+# 1. Finding the top 10 starting integers that produce the longest sequences
+
+# sorting length in descending order
+sorted_length_descending <- arrange(collatz_df, desc(length))
+
+# top 10 starting integers with longest sequence
+top10longest <- head(sorted_length_descending, 10)
+
+# view the top 10 starting integers with longest sequence
+top10longest
