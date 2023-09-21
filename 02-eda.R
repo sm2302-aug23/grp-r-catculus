9136eaa3aa79378cb03b2dafc89b061b51606cb0
AmeliaElam <20b2072@ubd.edu.bn>
2023-09-21 22:58:45 +0800

    Update on task 2, no. 2

--- 02-eda.R
+++ 02-eda.R
@@ -10,4 +10,16 @@ top10longest <- collatz_df %>%
   arrange(desc(length)) %>% # sorting in descending order
   head(10) # show only top 10 of the sorted desc. order
 
-# 2. 
+# 2. Finding out which starting integer produces a sequence that reaches the highest maximum value
+
+max_val_int <- collatz_df %>%
+  select(c(start,length)) %>%
+  arrange(desc(length)) %>% # sorting in descending order
+  head(1)
+
+# 3. Getting the average length and standard deviation of the sequence for even starting integers compared to odd ones?
+
+
+
+
+
