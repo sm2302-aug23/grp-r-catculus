436d6c8b3dd4e62274bb6f5c42bc78d94d025d46
syirahomar <20b2071@ubd.edu.bn>
2023-09-21 17:12:02 +0800

    update on task 2, no.1
    used data wrangling technique

--- 02-eda.R
+++ 02-eda.R
@@ -1,12 +1,10 @@
 # Task 2
 
+library(tidyverse)
+library(dplyr)
+
 # 1. Finding the top 10 starting integers that produce the longest sequences
 
-# sorting length in descending order
-sorted_length_descending <- arrange(collatz_df, desc(length))
-
-# top 10 starting integers with longest sequence
-top10longest <- head(sorted_length_descending, 10)
-
-# view the top 10 starting integers with longest sequence
-top10longest
+top10longest <- collatz_df %>%
+  arrange(desc(length)) %>% # sorting in descending order
+  head(10) # show only top 10 of the sorted desc. order
